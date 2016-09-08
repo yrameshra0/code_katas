179fe0c2d5ca9ff9330522be126caab094d73e69
Ramesh <yrameshrao@gmail.com>
2016-09-08 20:20:49 +0530

    Breadth first search for finding path between nodes

---
+++ graphkata/src/main/java/Node.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class Node {
+    public final String data;
+    public final List<Node> adjacent;
+    public boolean visited = false;
+
+    public Node(String data, List<Node> adjacent) {
+        this.data = data;
+        this.adjacent = adjacent;
+    }
+}
